20e044509a8dc93dde23a52175e1e56f09a3a6aa
Sujeet Poudel <lamar.sujeetpoudel@gmail.com>
2017-06-04 23:21:52 -0500

    temp

--- ads.c
+++ ads.c
@@ -11,6 +11,13 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 
+#define Tb 48
+#define Db 9650
+#define Tc 11
+#define Dc 24890
+
+float temp_calc(int D);
+
 int file;
 	
 	
@@ -29,26 +36,44 @@ int file;
 	// Select configuration register(0x01)
 	// AINP = AIN0 and AINN = AIN1, +/- 2.048V
 	// Continuous conversion mode, 128 SPS(0x84, 0x83)
-	char config[3] = {0};
-	config[0] = 0x01;
-	config[1] = 0x84;
-	config[2] = 0x83;
-	write(file, config, 3);
-	sleep(1);
+	
 	// Read 2 bytes of data from register(0x00)
 	// raw_adc msb, raw_adc lsb
 	
 	// Output data to screen
+	float f=0.0;
+	int D, a0, a1, a2, a3;
+	
 	while(1) {
 		
-	printf("Digital Value of Analog Input: %d \n", read_adc());
-}
+		a0 = read_adc(0);
+		a1 = read_adc(1);
+		a2 = read_adc(2);
+		a3 = read_adc(3);
+		
+		
+
+	printf("A0=%d A1=%d A2=%d A3=%d Temp=%f\n", a0,a1,a2,a3, temp_calc(a3));
+	
 	
 	}
 	
-	
+	}
+		
 
-int read_adc() {
+int read_adc(int channel) {
+	
+	char config[3] = {0};
+	config[0] = 0x01;
+	
+	if (channel== 0) config[1] = 0xD3;	
+	if (channel== 1) config[1] = 0xE3;	
+	if (channel== 2) config[1] = 0xF3;	
+	if (channel== 3) config[1] = 0xC3;
+   
+
+	config[2] = 0x83;
+	write(file, config, 3);
 	
 	char reg[1] = {0x00};
 	write(file, reg, 1);
@@ -61,10 +86,25 @@ int read_adc() {
 	{
 	// Convert the data
 	int raw_adc = (data[0] * 256 + data[1]);
-	if (raw_adc > 32767)
-	{
-	raw_adc -= 65535;
-	}
+	if  (raw_adc >32767) raw_adc =0; 
+    delay(1000);
 	return raw_adc;
 	}
+
+}
+
+void delay(int k) {
+	int i=0;
+	while(k>0) {
+	for(i =0; i<=100000; i++);
+	k=k-1;
+    }
+}
+
+
+float temp_calc(int D) {
+	
+float f= (1.0*(Tb-Tc)*(D-Dc)/(Db-Dc)+Tc);
+printf("\n T = %f   %d\n",f, D);
+return f;
 }
